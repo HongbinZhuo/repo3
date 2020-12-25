2e3c89f952ed400c7783a3180ecd7c7996f5f27f
hongbinzhuo <1178550987@qq.com>
2020-12-25 20:19:32 +0800

    提交

---
+++ test/src/com/demo/oo.java
@@ -0,0 +1,24 @@
+package com.demo;
+
+/**
+ * @Author Zhuohongbin
+ * @Description TODO
+ * @Date 2020/12/25 20:16
+ */
+public class oo {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa");
+    }
+}
